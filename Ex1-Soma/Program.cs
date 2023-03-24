9b360201c818afb8d8f5676ed8e7402cdb173bb8
Francine-Oliveira <olive.fran86@gmail.com>
2023-03-23 21:31:37 -0300

    resposta 1

---
+++ Ex1-Soma/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex1_Soma;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int indice = 13, soma = 0, K = 0;
+
+        while (K < indice)
+        {
+            K++;
+            soma += K;
+        }
+
+        Console.WriteLine(soma);
+
+        Console.ReadKey();
+    }
+}
+
